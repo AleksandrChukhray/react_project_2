caecc4ce0fd91855f7aef905168ea0d43281af3e
Alex Chukhrai <zimmalex6@gmail.com>
2016-11-07 15:37:31 +0200

    test commit

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var express = require('express'),
 app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(__dirname));
 app.listen(app.get('port'), function() {
-    console.log('Server started: http://localhost:' + app.get('port') + '/');
+    console.log('Server starte: http://localhost:' + app.get('port') + '/');
 });
